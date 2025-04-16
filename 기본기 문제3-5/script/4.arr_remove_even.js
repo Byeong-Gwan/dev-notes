25cfedcf0a202c7ad4e7e114cc661ae5e7787831
김병관 <ansd_my3073@naver.com>
2025-04-16 10:30:59 +0900

    [기본기 문제3-5][4]배열에서 짝수 제거하기
    for 문에서 filter 사용으로 변경

--- 기본기 문제3-5/script/4.arr_remove_even.js
+++ 기본기 문제3-5/script/4.arr_remove_even.js
@@ -9,11 +9,5 @@
 console.log(evenRemove([1, 2, 3, 4, 5, 6]));
 
 function evenRemove (arr) {
-    const result = arr.forEach((e) => {
-        if (e % 2 === 0) {
-            e.pop();
-        }
-        console.log('arr', arr);
-    });
-    return result;
+    return arr.filter(n => n % 2 !== 0);
 }
