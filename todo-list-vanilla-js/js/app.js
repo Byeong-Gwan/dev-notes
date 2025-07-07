9ad9d3230aea06d055a44f7a13c6c7a865840266
김병관 <ansd_my3073@naver.com>
2025-07-07 17:25:26 +0900

    [개인 프로젝트][리펙토링] 기존 구조에서 ES6버전으로 구조 변경 리팩토링 진행

--- todo-list-vanilla-js/js/app.js
+++ todo-list-vanilla-js/js/app.js
@@ -1,58 +1,63 @@
-;(function(context) {
+import { $, qsa, each, pluralization, on, delegate, parent, nodeListEach } from './helpers.js';
 
-  'use strict';
+// 키보드의 특정 키가 눌렸을 때 동작을 제어하기 위한 키 코드값 상수 선언
+const ENTER_KEY = 13;
+const ESC_KEY = 27;
 
-  var ENTER_KEY = 13;
-  var ESC_KEY = 27;
+// 앱의 초기 상태를 세팅하고, 로컬스토리지에 저장되는 데이터를 관리하는 구조를 포함
+class App{
+  constructor ( localStorageKey ) {
 
-  function App( localStorageKey ) {
-
+    // Stores라는 별도 객체가 로컬스토리지에 데이터를 저장/조회/삭제하는 역할을 담당
     this.stores = new Stores(localStorageKey);
-    this.currentId = 0;
+    this.currentId = 0; // 현재 수정 중인 아이템 ID 등 추적 용도로 사용
+    
+    // DOM 요소 연결
     this.$insert = $('#js-insert');
-    this.$toggleAll = $('#js-toggle-all');
+    this.$toggleAll = $('#js-toggle-all'); 
     this.$bar = $('#js-bar');
     this.$list = $('#js-list');
     this.$clearCompleted = $('#js-clear-completed');
     this.$total = $('#js-total');
     this.$filters = $('#js-filters');
-    this.addEventListeners();
-    this.render();
-
+  
+  
+    this.addEventListeners(); // 입력, 버튼 클릭 등 이벤트 연결
+    this.render(); // 화면 초기 렌더링 실행
+  
   }
 
-  App.fn = App.prototype;
 
-  App.fn.addEventListeners = function() {
+  addEventListeners () {
 
-    $.on(this.$insert, 'keypress', this.onInsert.bind(this));
+    on(this.$insert, 'keypress', this.onInsert.bind(this));
 
-    $.on(this.$toggleAll, 'click', this.onToggleAll.bind(this));
-    $.delegate(this.$list, '.toggle', 'click', this.onToggle.bind(this));
+    on(this.$toggleAll, 'click', this.onToggleAll.bind(this));
+    delegate(this.$list, '.toggle', 'click', this.onToggle.bind(this));
 
-    $.delegate(this.$list, '.destroy', 'click', this.onDestroy.bind(this) );
+    delegate(this.$list, '.destroy', 'click', this.onDestroy.bind(this) );
 
-    $.on(this.$clearCompleted, 'click', this.onClearCompleted.bind(this));
+    on(this.$clearCompleted, 'click', this.onClearCompleted.bind(this));
 
 
-    $.delegate(this.$filters, '.button', 'click', this.onFilter.bind(this));
+    delegate(this.$filters, '.button', 'click', this.onFilter.bind(this));
 
-    $.delegate(this.$list, 'span', 'dblclick', this.onStartEditing.bind(this));
-    $.delegate(this.$list, '.edit', 'keyup', this.onEditingCancel.bind(this));
-    $.delegate(this.$list, '.edit', 'keypress', this.onEditingDone.bind(this));
-    $.delegate(this.$list, '.edit', 'blur', this.onEditingLeave.bind(this) );
+    delegate(this.$list, 'span', 'dblclick', this.onStartEditing.bind(this));
+    delegate(this.$list, '.edit', 'keyup', this.onEditingCancel.bind(this));
+    delegate(this.$list, '.edit', 'keypress', this.onEditingDone.bind(this));
+    delegate(this.$list, '.edit', 'blur', this.onEditingLeave.bind(this) );
   };
 
-  App.fn.onStartEditing = function(event) {
-    var li = $.parent(event.target, 'li');
-    var element = $('.edit', li);
+  onStartEditing (event) {
+    const li = parent(event.target, 'li');
+    const element = $('.edit', li);
     this.currentId = parseInt(li.dataset.id, 10);
     li.className += ' editing';
     element.value = event.target.innerHTML;
     element.focus();
   };
 
-  App.fn.onEditingCancel = function(event) {
+  onEditingCancel (event) {
     if( event.keyCode === ESC_KEY ) {
       console.log('onEditingCancel', event.target);
       event.target.dataset.isCanceled = true;
@@ -60,20 +65,20 @@
     }
   };
 
-  App.fn.onEditingDone = function(event) {
+  onEditingDone (event) {
     if( event.keyCode === ENTER_KEY ) {
       event.target.blur();
     }
   };
 
-  App.fn.onEditingLeave = function(event) {
+  onEditingLeave (event) {
     console.log('onEditingLeave');
-    var input = event.target;
-    var id = this.getItemId( input );
-    var text = input.value.trim();
-    var li = this.getElementByDataId( id );
+    const input = event.target;
+    const id = this.getItemId( input );
+    const text = input.value.trim();
+    const li = this.getElementByDataId( id );
     if( input.value.trim() ) {
-      var item = {
+      let item = {
         id: id,
         text: text
       };
@@ -87,7 +92,7 @@
     }
   };
 
-  App.fn.endEditing = function( li, text ) {
+  endEditing ( li, text ) {
     li.className = li.className.replace('editing', '');
     $('.edit', li).removeAttribute('data-is-canceled');
     if( text ) {
@@ -95,27 +100,27 @@
     }
   };
 
-  App.fn.getItemId = function( element ) {
-    var li = $.parent(element, 'li');
+  getItemId ( element ) {
+    const li = parent(element, 'li');
     return parseInt(li.dataset.id, 10);
   };
 
-  App.fn.getElementByDataId = function( id ) {
+  getElementByDataId ( id ) {
     return $('[data-id="' + id + '"]');
   };
 
-  App.fn.onInsert = function( event ) {
-    var element = event.target;
-    var text = element.value.trim();
+  onInsert ( event ) {
+    let element = event.target;
+    const text = element.value.trim();
     if( text && event.keyCode === ENTER_KEY ) {
       this.insert(text);
       element.value = '';
     }
   };
 
-  App.fn.onToggleAll = function(event) {
-    var checked = event.target.checked;
-    var self = this;
+  onToggleAll (event) {
+    const checked = event.target.checked;
+    const self = this;
 
     this.stores.findAll(function( items ) {
       $.each( items, function( item ) {
@@ -126,28 +131,28 @@
     });
   };
 
-  App.fn.onToggle = function(event) {
-    var element = event.target;
-    var id = this.getItemId( element );
-    var item = {
+  onToggle (event) {
+    const element = event.target;
+    const id = this.getItemId( element );
+    const item = {
       id: id,
       completed: element.checked
     };
     this.stores.save( item, function(item) {
-      var li = this.getElementByDataId( item.id );
+      const li = this.getElementByDataId( item.id );
       li.className = item.completed ? 'completed' : '';
       $('.toggle', li).checked = item.completed;
       this.showControls();
     }.bind(this));
   };
 
-  App.fn.onDestroy = function(event) {
-    var id = this.getItemId( event.target );
+  onDestroy (event) {
+    const id = this.getItemId( event.target );
     this.destroy( id );
   };
 
-  App.fn.onClearCompleted = function(event) {
-    var self = this;
+  onClearCompleted (event) {
+    const self = this;
     this.stores.findAll(function( items ) {
       items = items
         .filter(function( item ) {
@@ -159,52 +164,53 @@
     });
   };
 
-  App.fn.onFilter = function(event) {
+  onFilter (event) {
     document.location.hash = event.target.getAttribute('href');
     this.render();
   };
 
   // Insert
-  App.fn.insert = function( text ) {
-    var item = {
+  insert ( text ) {
+    const item = {
       text: text,
       completed: false
     };
     this.stores.save(item, function( item ) {
-      var element = this.nodeItem( item );
+      const element = this.nodeItem( item );
       this.$list.appendChild( element );
       this.showControls();
     }.bind(this));
   };
 
   // Destroy
-  App.fn.destroy = function( id ) {
+  destroy ( id ) {
     this.stores.destroy( id, function() {
-      var li = this.getElementByDataId( id );
+      const li = this.getElementByDataId( id );
       this.$list.removeChild(li);
       this.showControls();
     }.bind(this));
   };
 
-  App.fn.filter = function() {
-    var hash = document.location.hash;
+  filter () {
+    const hash = document.location.hash;
+    const buttons = qsa('.button', this.$filters);
     if( !hash ) return false;
-    $.qsa( '.button', this.$filters )
-      .each(function( button ) {
-        if ( button.getAttribute('href') === hash ) {
-          button.className = 'button selected';
-        } else {
-          button.className = button.className.replace('selected', '');
-        }
-      });
-    hash = hash.split('#/')[1]
-    return hash !== 'all' ? hash : false;
+    nodeListEach(buttons, (button) => {
+      if(button.getAttribute('href') === hash) {
+        button.className = 'button selected';
+      } else {
+        button.className = button.className.replace('selected', '');
+      }
+    });
+
+    const filterHash = hash.split('#/')[1]
+    return filterHash !== 'all' ? filterHash : false;
   };
 
   // Render
-  App.fn.render = function() {
+  render () {
 
-    var filter = this.filter();
+    const filter = this.filter();
 
     this.stores.findAll(function(items){
       if( filter ) {
@@ -213,7 +219,7 @@
             return item.completed === ( filter === 'completed' );
           });
       }
-      var nodes = this.nodeItemMulti( items );
+      const nodes = this.nodeItemMulti( items );
       this.$list.innerHTML = "";
       this.$list.appendChild(nodes);
       this.showControls();
@@ -223,7 +229,7 @@
   };
 
 
-  App.fn.showControls = function () {
+  showControls () {
     this.stores.findAll(function(items){
       this.showBarAndToggleAll( items );
       this.showTotalTasksLeft( items );
@@ -231,50 +237,50 @@
     }.bind(this));
   };
 
-  App.fn.showBarAndToggleAll = function( items ) {
-    var total = items.length;
-    var completed = items.filter(function(item){
+  showBarAndToggleAll ( items ) {
+    const total = items.length;
+    const completed = items.filter(function(item){
       return item.completed;
     });
-    var value = total ? 'block' : 'none';
+    const value = total ? 'block' : 'none';
     this.$toggleAll.style.display = value;
     this.$toggleAll.checked = total === completed.length;
     this.$bar.style.display = value;
   };
 
-  App.fn.showTotalTasksLeft = function(items) {
+  showTotalTasksLeft (items) {
     items = items
       .filter(function( item ) {
         return !item.completed;
       });
-    var len = items.length;
-    var text = [len,' item',$.pluralization( len ),' left'].join('');
+    const len = items.length;
+    const text = [len,' item', pluralization( len ),' left'].join('');
     this.$total.innerHTML = text;
   };
 
-  App.fn.showClearCompleted = function(items) {
-    var some = items
+  showClearCompleted (items) {
+    const some = items
       .some(function( item ) {
         return item.completed;
       });
     this.$clearCompleted.style.display = some ? 'inline-block' : 'none';
   };
 
-  App.fn.nodeItemMulti = function ( items ) {
-    var fragment = document.createDocumentFragment();
-    $.each( items, function( item ) {
+  nodeItemMulti ( items ) {
+    const fragment = document.createDocumentFragment();
+    each( items, function( item ) {
       fragment.appendChild( this.nodeItem(item) );
     }.bind(this));
     return fragment;
   };
 
-  App.fn.nodeItem = function( item ) {
-    var li = document.createElement('li');
-    var div = document.createElement('div');
-    var toggle = document.createElement('input');
-    var span = document.createElement('span');
-    var destroy = document.createElement('button');
-    var edit = document.createElement('input');
+  nodeItem ( item ) {
+    const li = document.createElement('li');
+    const div = document.createElement('div');
+    const toggle = document.createElement('input');
+    const span = document.createElement('span');
+    const destroy = document.createElement('button');
+    const edit = document.createElement('input');
 
     li.setAttribute('data-id', item.id );
 
@@ -306,11 +312,10 @@
 
     return li;
   }
+}
+// Initialization on Dom Ready
+window.addEventListener('DOMContentLoaded', function() {
+  const app = new App('todo');
+});
 
-  // Initialization on Dom Ready
-  window.addEventListener('DOMContentLoaded', function() {
-    var app = new App('todo');
-  });
-
-})(this);
 
--- todo-list-vanilla-js/js/helpers.js
+++ todo-list-vanilla-js/js/helpers.js
@@ -1,84 +1,69 @@
-;(function(context) {
 
-  'use strict';
+// 선택자(selector)를 기준으로 DOM 요소를 찾는 기능
+export function $ ( selector, scope ) {
+  return qsa( selector, scope, true );
+}
 
-  function $( selector, scope ) {
-    return $.qsa( selector, scope, true );
+export function qsa ( selector, scope, first ) {
+  const elementList = ( scope || document).querySelectorAll( selector );
+  return first ? elementList[0] : elementList;
+};
+
+export function noop () {};
+
+
+export function each ( array, cb ) {
+  if (!array || !array.length) return;
+
+  for (let i = 0; i < array.length; i++) {
+    cb.call(array, array[i], i, array);
+  }
+};
+
+export function pluralization ( value ) {
+  return Number(value) === 1 ? "" : "s";
+};
+
+export function on ( target, type, callback, useCapture ) {
+  target.addEventListener(type, callback, !!useCapture);
+};
+
+export function delegate ( target, selector, type, handler ) {
+  function dispatchEvent(event) {
+   const potentialMatch = event.target.closest(selector);
+   if (potentialMatch && target.contains(potentialMatch)) {
+    handler.call(potentialMatch, event);
+   }
   }
 
-  $['qsa'] = function( selector, scope, first ) {
-    var e = ( scope || document).querySelectorAll( selector );
-    return first ? e[0] : e;
-  };
+  // https://developer.mozilla.org/en-US/docs/Web/Events/blur
+  const useCapture = type === 'blur' || type === 'focus';
 
-  $['noop'] = function() {};
+  on(target, type, dispatchEvent, useCapture);
+};
 
-
-  $['each'] = function( array, cb ) {
-    var len = array.length;
-    var idx = -1;
-    while( ++idx < len ) {
-      cb.call(array, array[idx], idx, array);
-    }
-  };
-
-  $['pluralization'] = function( value ) {
-    return +value === 1 ? "" : "s";
-  };
-
-  $['on'] = function (target, type, callback, useCapture) {
-    target.addEventListener(type, callback, !!useCapture);
-  };
-
-  $['delegate'] = function (target, selector, type, handler) {
-    function dispatchEvent(event) {
-      var targetElement = event.target;
-      var potentialElements = $.qsa(selector, target);
-      var hasMatch = Array.prototype.indexOf.call(potentialElements, targetElement) >= 0;
-      if (hasMatch) {
-        handler.call(targetElement, event);
-      }
-    }
-
-    // https://developer.mozilla.org/en-US/docs/Web/Events/blur
-    var useCapture = type === 'blur' || type === 'focus';
-
-    $.on(target, type, dispatchEvent, useCapture);
-  };
-
-  $['parent'] = function (element, tagName) {
-    if (!element.parentNode) {
-      return;
-    }
+export function parent ( element, tagName ) {
+  while (element && element.parentNode) {
     if (element.parentNode.tagName.toLowerCase() === tagName.toLowerCase()) {
       return element.parentNode;
     }
-    return $.parent(element.parentNode, tagName);
-  };
-
-  context.$ = $;
-
-})(this);
+    element = element.parentNode;
+  }
+  return null;
+};
 
 // Prototype
-NodeList.prototype.each = function( fn ) {
-  var len = this.length;
-  var idx = -1;
-  while( ++idx < len ) {
-    fn.call(this, this[idx], idx, this);
+// NodeList.prototype.each = function( fn ) {
+//   const len = this.length;
+//   let idx = -1;
+//   while( ++idx < len ) {
+//     fn.call(this, this[idx], idx, this);
+//   }
+// }
+
+export function nodeListEach (nodeList, cb) {
+  for (let i = 0; i < nodeList.length; i++) {
+    cb.call(nodeList, nodeList[i], i, nodeList);
   }
 }
-
-Array.prototype.some = function( fn ) {
-
-  var len = this.length;
-  var idx = -1;
-  while( ++idx < len ) {
-    if( fn( this[idx], idx, this ) ) {
-      return true;
-    }
-  }
-  return false;
-
-};
 
